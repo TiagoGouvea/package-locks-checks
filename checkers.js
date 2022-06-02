d34566c886380d1c2f620761cfb89e5318e8fc9a
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2022-06-02 09:27:30 -0300

    Avoid integrity check when have not a yarn.lock

---
+++ checkers.js
@@ -0,0 +1,126 @@
+import fs from "fs";
+import {spawn} from "child_process";
+import semver from "semver";
+
+function getPackageContent(packageFile) {
+    if (fs.existsSync(packageFile)) {
+        const filecontent = fs.readFileSync(packageFile, {encoding: 'utf-8'});
+        return JSON.parse(filecontent);
+    }
+}
+
+export function checkMultipleLockFiles(options) {
+    const lockFiles = [fs.existsSync(options.basePath + 'package-lock.json'), fs.existsSync(options.basePath + 'yarn.lock'), fs.existsSync(options.basePath + 'pnpm-lock.yaml')];
+    const multipleExisting = lockFiles.filter(l => l == true).length > 1;
+    if (options.debug) console.log("bothExists", multipleExisting);
+    if (multipleExisting) {
+        console.log(`Multiple package lockers existing at same time is NOT OK.`);
+        console.log(`In order to fix this do:`);
+        console.log(`- Remove one of that files`);
+        console.log(`- Run ${options.packageManager} again to update lock files`);
+        return 1; // error
+    }
+
+    return 0;
+}
+
+export function checkUnmatchedYarnLockFile(options) {
+    return new Promise((resolve, reject) => {
+        try {
+            if (options.debug) console.log("basePath", options.basePath);
+            if (!fs.existsSync(options.basePath + 'yarn.lock')){
+                console.log("yarn.lock not found, cannot check for integrity. You can use '--allowUnmatchedYarnLock true' to skip this verification");
+                return resolve(0);
+            }
+            let out = '';
+            let outError = '';
+            const close = (code) => {
+                if (options.debug) console.log(">> checkUnmatchedLockFile >> close - code: ", code, " - out: ", out, " - outError: ", outError);
+                if (out.includes("Folder in sync")) {
+                    resolve(0);
+                } else if (outError.includes("Couldn't find an integrity file")) {
+                    console.log(outError);
+                    // console.log("Couldn't find an integrity file - node_modules folder could be not present")
+                    resolve(1);
+                } else if (outError.includes("Lockfile does not contain pattern")) {
+                    console.log("package.json and yarn.lock has unmatched versions")
+                    console.log(outError);
+                    // console.log("Couldn't find an integrity file - node_modules folder could be not present")
+                    resolve(1);
+                } else {
+                    console.log("Unexpected yarn check integrity (checkUnmatchedLockFile) error:");
+                    console.log(">> Code: ", code, " - Error message: ", outError, " Additional yarn message: ", out);
+                    resolve(1);
+                }
+            }
+
+            const spawn = require('child_process').spawn;
+
+            // if (options.debug) console.log("options", options);
+            // yarn check --integrity
+
+            const childProcess = spawn(options.packageManager, ['check', '--integrity'], {
+                cwd: options.basePath,
+            });
+            childProcess.on('close', function (code) {
+                close(code);
+            }).on('exit', function (code) {
+                // if (options.debug) console.log(">> exit code", code);
+                // if (code != 0)
+                //     close(code);
+            });
+            childProcess.stderr.on('data', (data) => {
+                if (options.debug) console.log(">> stderr data", data.toString());
+                outError += data;
+            });
+            childProcess.stdout.on('data', function (data) {
+                if (options.debug) console.log(">> data", data.toString());
+                out += data;
+            });
+        } catch (err) {
+            console.log(">> catch error", err);
+            reject(1);
+        }
+    })
+}
+
+export function checkRange(options, content) {
+    if (!content)
+        content = getPackageContent(options.packageFile);
+    // const find = "*";
+    if (options.debug) console.log("content", content);
+
+    const dependencies = {...content.dependencies, ...content.devDependencies};
+    if (options.debug) console.log("dependencies", dependencies);
+
+    const packages = Object.keys(dependencies);
+    if (options.debug) console.log("packages", packages);
+
+    const exists = packages.reduce((result, pkg) => {
+        const packageValue = dependencies[pkg];
+        const version = semver.valid(packageValue);
+        const range = semver.validRange(packageValue);
+
+        if (options.debug) console.log(pkg, packageValue, "- range", range, " - version", version);
+
+        if (!version)
+            result.push(`"${pkg}": "${packageValue}"`);
+        return result;
+    }, []);
+    if (options.debug) console.log("exits", exists);
+
+    if (exists.length > 0) {
+        console.log(`Package is NOT OK. It includes dependencies with wide version ranges:`)
+        exists.map(pkg => {
+            console.log(pkg)
+        })
+        console.log();
+        console.log(`In order to fix this do:`);
+        console.log(`- Update dependencies on package removing ranges, restricting acceptable version ranges`);
+        console.log(`- Run <yarn> again to update lock files`);
+        return 1; // error
+    }
+
+    // console.log(`Package is OK`);
+    return 0; // ok
+}
--- index.js
+++ index.js
@@ -3,126 +3,6 @@
 const fs = require('fs');
 const path = require('path');
 const {program} = require('commander');
-const semver = require("semver");
-
-function getPackageContent(packageFile) {
-    if (fs.existsSync(packageFile)) {
-        const filecontent = fs.readFileSync(packageFile, {encoding: 'utf-8'});
-        return JSON.parse(filecontent);
-    }
-}
-
-function checkMultipleLockFiles(options) {
-    const lockFiles = [fs.existsSync(options.basePath + 'package-lock.json'), fs.existsSync(options.basePath + 'yarn.lock'), fs.existsSync(options.basePath + 'pnpm-lock.yaml')];
-    const multipleExisting = lockFiles.filter(l => l == true).length > 1;
-    if (options.debug) console.log("bothExists", multipleExisting);
-    if (multipleExisting) {
-        console.log(`Multiple package lockers existing at same time is NOT OK.`);
-        console.log(`In order to fix this do:`);
-        console.log(`- Remove one of that files`);
-        console.log(`- Run ${options.packageManager} again to update lock files`);
-        return 1; // error
-    }
-
-    return 0;
-}
-
-function checkUnmatchedYarnLockFile(options) {
-    return new Promise((resolve, reject) => {
-        try {
-            if (options.debug) console.log("basePath", options.basePath);
-            let out = '';
-            let outError = '';
-            const close = (code) => {
-                if (options.debug) console.log(">> checkUnmatchedLockFile >> close - code: ", code, " - out: ", out, " - outError: ", outError);
-                if (out.includes("Folder in sync")) {
-                    resolve(0);
-                } else if (outError.includes("Couldn't find an integrity file")) {
-                    console.log(outError);
-                    // console.log("Couldn't find an integrity file - node_modules folder could be not present")
-                    resolve(1);
-                } else if (outError.includes("Lockfile does not contain pattern")) {
-                    console.log("package.json and yarn.lock has unmatched versions")
-                    console.log(outError);
-                    // console.log("Couldn't find an integrity file - node_modules folder could be not present")
-                    resolve(1);
-                } else {
-                    console.log("Unexpected yarn check integrity (checkUnmatchedLockFile) error:");
-                    console.log(">> Code: ", code, " - Error message: ", outError, " Additional yarn message: ", out);
-                    resolve(1);
-                }
-            }
-
-            const spawn = require('child_process').spawn;
-
-            // if (options.debug) console.log("options", options);
-            // yarn check --integrity
-
-            const childProcess = spawn(options.packageManager, ['check', '--integrity'], {
-                cwd: options.basePath,
-            });
-            childProcess.on('close', function (code) {
-                close(code);
-            }).on('exit', function (code) {
-                // if (options.debug) console.log(">> exit code", code);
-                // if (code != 0)
-                //     close(code);
-            });
-            childProcess.stderr.on('data', (data) => {
-                if (options.debug) console.log(">> stderr data", data.toString());
-                outError += data;
-            });
-            childProcess.stdout.on('data', function (data) {
-                if (options.debug) console.log(">> data", data.toString());
-                out += data;
-            });
-        } catch (err) {
-            console.log(">> catch error", err);
-            reject(1);
-        }
-    })
-}
-
-function checkRange(options, content) {
-    if (!content)
-        content = getPackageContent(options.packageFile);
-    // const find = "*";
-    if (options.debug) console.log("content", content);
-
-    const dependencies = {...content.dependencies, ...content.devDependencies};
-    if (options.debug) console.log("dependencies", dependencies);
-
-    const packages = Object.keys(dependencies);
-    if (options.debug) console.log("packages", packages);
-
-    const exists = packages.reduce((result, pkg) => {
-        const packageValue = dependencies[pkg];
-        const version = semver.valid(packageValue);
-        const range = semver.validRange(packageValue);
-
-        if (options.debug) console.log(pkg, packageValue, "- range", range, " - version", version);
-
-        if (!version)
-            result.push(`"${pkg}": "${packageValue}"`);
-        return result;
-    }, []);
-    if (options.debug) console.log("exits", exists);
-
-    if (exists.length > 0) {
-        console.log(`Package is NOT OK. It includes dependencies with wide version ranges:`)
-        exists.map(pkg => {
-            console.log(pkg)
-        })
-        console.log();
-        console.log(`In order to fix this do:`);
-        console.log(`- Update dependencies on package removing ranges, restricting acceptable version ranges`);
-        console.log(`- Run <yarn> again to update lock files`);
-        return 1; // error
-    }
-
-    // console.log(`Package is OK`);
-    return 0; // ok
-}
 
 program
     .version(require('./package.json').version)
@@ -139,7 +19,6 @@ program
 const options = program.opts();
 
 async function doVerify() {
-
     const exitCodes = [];
 
     // Normalize params
@@ -148,6 +27,8 @@ async function doVerify() {
     if (!options.basePath && options.packageFile) options.basePath = path.dirname(options.packageFile);
     if (!options.basePath) options.basePath = process.cwd(); //path.normalize('./');
     if (!options.packageFile) options.packageFile = path.normalize(options.basePath + path.sep + 'package.json');
+
+    console.log("package-locks-checks > validating package with "+options.packageManager);
 
     // Show normalized options (when debugging)
     if (options.debug) console.log("options", options);
@@ -190,7 +71,7 @@ async function doVerify() {
 
     const finalExitCode = Math.max(...exitCodes);
     if (finalExitCode == 0) {
-        console.log("package-locks-checks: Everything is ok!");
+        console.log("package-locks-checks > Everything is ok!");
     }
     process.exit(finalExitCode);
 }
